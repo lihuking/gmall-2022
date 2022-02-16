b5e724d82b7e95f062449d1c8862d6b91eeec46d
李虎 <lihuking2021@aliyun.com>
2022-02-16 14:18:50 +0800

    更新一

---
+++ gmall-parent/gmall-interface/src/main/java/com/lihuking/gmall/service/UserInfoService.java
@@ -0,0 +1,8 @@
+package com.lihuking.gmall.service;
+
+import java.util.List;
+import com.lihuking.gmall.bean.UserInfo;
+public interface UserInfoService {
+
+   List<UserInfo>  getUserInfoAll();
+}
